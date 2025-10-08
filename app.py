faf409aaf695be08068cfd088af24d4d3d29a381
Ahsan Zafar <81752727+Ahsan-Zafar66@users.noreply.github.com>
2025-10-08 12:29:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Changing from github and seeing if pipeline triggers automatically"})
+    return jsonify({"message": "Changing from github and seeing if pipeline triggers automatically. Trying again"})
 
 @app.route('/health')
 def health():
